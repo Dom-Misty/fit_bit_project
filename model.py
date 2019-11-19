a3ef812cf6da0e74769f3959c15c4429c807c91e
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-11-19 15:19:11 -0600

    added fnctions for modeling time series

---
+++ model.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+
+from datetime import datetime
+import itertools as it
+
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
+import statsmodels.api as sm
+from statsmodels.tsa.ar_model import AR
+
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.metrics import mean_squared_error
+
+import math
+
+import matplotlib.pyplot as plt
+
+
+def last_value(train, test):
+    last_value = train[-1]
+    yhat = [last_value] * test.shape[0]
+    mse = mean_squared_error(test, yhat)
+    rmse = mse**.5
+    return {'model_type': 'last_value', 
+            'mse': mse,
+            'rmse': rmse}
+
+def simple_avg(train, test):
+    simple_average = train.mean()
+    yhat = [simple_average] * test.shape[0]
+    mse = mean_squared_error(test, yhat)
+    rmse = mse**.5
+    return {'model_type': 'simple_avg', 
+            'mse': mse,
+            'rmse': rmse}
+
+def moving_avg(train, test):
+    moving_average = train.rolling(30).mean()[-1]
+    yhat = [moving_average] * test.shape[0]
+    mse = mean_squared_error(test, yhat)
+    rmse = mse**.5
+    return {'model_type': 'moving_avg', 
+            'mse': mse,
+            'rmse': rmse}
+
+def linear_holt(train,test):
+    lin_h = Holt(train).fit()
+    yhat = lin_h.forecast(test.shape[0])
+    mse = mean_squared_error(test, yhat)
+    rmse = mse**.5
+    return {'model_type': 'holt', 
+            'mse': mse,
+            'rmse': rmse}
+
+def run_models(train,test):
+    model_strength = pd.DataFrame(columns=['model_type', 'mse', 'rmse'])
+
+    row = last_value(train,test)
+    model_strength = model_strength.append(row, ignore_index= True)
+
+    row = simple_avg(train,test)
+    model_strength = model_strength.append(row, ignore_index= True)
+
+    row = moving_avg(train,test)
+    model_strength = model_strength.append(row, ignore_index= True)
+
+    row = linear_holt(train,test)
+    model_strength = model_strength.append(row, ignore_index= True)
+
+    return model_strength
