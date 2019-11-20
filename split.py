1b3eaca9020ae639c5670d345fb2793c58df649b
Misty <ms.misty.garcia@gmail.com>
2019-11-20 11:23:25 -0600

    add weekly split

--- split.py
+++ split.py
@@ -1,7 +1,12 @@
 import pandas as pd 
 import numpy as np 
 
-def split_two_weeks(df, resample):
-    train = df[:'2018-11-22'].resample(resample).agg(sum)
-    test = df['2018-11-23':].resample(resample).agg(sum)
+def split_daily(df):
+    train = df[:'2018-11-22'].resample("D").agg("mean")
+    test = df['2018-11-23':].resample("D").agg("mean")
+    return train, test
+
+def split_weekly(df):
+    train = df[:'2018-11-25'].resample("W").agg("mean")
+    test = df['2018-12-02':].resample("W").agg("mean")
     return train, test
